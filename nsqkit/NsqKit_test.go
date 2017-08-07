1ad50a6980a09ca7532d46c7fa2ced7f9b5c3999
dongyunlong <dongyunlong@gumpcome.com>
2017-08-07 20:55:57 +0800

    增加操作NSQ消息服务客户端

---
+++ nsqkit/NsqKit_test.go
@@ -0,0 +1,44 @@
+package nsqkit
+
+import (
+	"testing"
+	"github.com/nsqio/go-nsq"
+	"fmt"
+	"sync"
+)
+
+func TestInitNsqClient(t *testing.T) {
+	nsqClient := &NsqClient{}
+	nsqConfig := &NsqConfig{}
+	nsqClient.Init("http://123.206.79.254:4761", nsqConfig)
+}
+
+func TestSendMessage(t *testing.T) {
+	nsqClient := &NsqClient{}
+	nsqConfig := &NsqConfig{}
+	nsqClient.Init("http://123.206.79.254:4761", nsqConfig)
+	nsqClient.CreateProducer()
+	for  {
+		nsqClient.SendMessage("write_test", "test")
+	}
+}
+
+func TestCreateConsumer(t *testing.T)  {
+	count := 0
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	nsqClient := &NsqClient{}
+	nsqConfig := &NsqConfig{}
+	nsqClient.Init("http://123.206.79.254:4761", nsqConfig)
+	nsqClient.CreateConsumer("write_test", "test", func(message *nsq.Message) error {
+		count++
+		fmt.Printf("%v-%v\n", count, string(message.Body))
+		return nil
+	})
+	wg.Wait()
+}
+
+func HandleJsonMessage(message *nsq.Message) error {
+	fmt.Println(string(message.Body))
+	return nil
+}
