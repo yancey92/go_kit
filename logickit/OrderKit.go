619d10fd2549bbc649bb5092b2e10f57b28d4c40
dongyunlong <dongyunlong@gumpcome.com>
2017-08-12 12:01:52 +0800

    订单工具增加根据订单号获取订单生成年份方法

--- logickit/OrderKit.go
+++ logickit/OrderKit.go
@@ -27,3 +27,28 @@ func GetTabelInfoByOutTradeNo(outTradeNo string) (string, string, string, string
 
 	return orderTable, orderMonth, orderDay, orderTime, nil
 }
+
+// 根据订单号获取分表信息
+// @outTradeNo 商户订单号
+// 返回值
+// 1:订单存储对应的表名
+// 2:生成订单的年份 YYYY
+// 3:生成订单的月份 YYYYMM
+// 4:生成订单的日期 YYYYMMDD
+func GetTabelInfoWithYearByOutTradeNo(outTradeNo string) (string, string, string, string, error) {
+	// 订单时间
+	orderTime := strkit.SubStr(outTradeNo, 0, 14)
+	if orderTime == "" || len(orderTime) != 14 {
+		return "", "", "", "", logiccode.New(120017, "依据订单号获取日期错误outTradeNo="+outTradeNo)
+	}
+	// 订单年份
+	orderYear := strkit.SubStr(outTradeNo, 0, 4)
+	// 订单月份
+	orderMonth := strkit.SubStr(outTradeNo, 0, 6)
+	// 订单日期
+	orderDay := strkit.SubStr(outTradeNo, 0, 8)
+	// 订单表
+	orderTable := "svm_trade_" + orderMonth
+
+	return orderTable, orderYear, orderMonth, orderDay, nil
+}
