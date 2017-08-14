be2fb9198f26507f768c1b2138bdd55eb70405c3
hetaotao <hetaotao@gumpcome.com>
2017-08-14 10:34:57 +0800

    新增中控获取订单分表信息，退款分表信息

--- logickit/OrderKit.go
+++ logickit/OrderKit.go
@@ -52,3 +52,54 @@ func GetTabelInfoWithYearByOutTradeNo(outTradeNo string) (string, string, string
 
 	return orderTable, orderYear, orderMonth, orderDay, nil
 }
+
+
+// 根据订单号获取分表信息
+// @outTradeNo 商户订单号
+// 返回值
+// 1:中控存储订单对应的表名
+// 2:生成订单的年份 YYYY
+// 3:生成订单的月份 YYYYMM
+// 4:生成订单的日期 YYYYMMDD
+func GetPayTabelInfoWithYearByOutTradeNo(outTradeNo string) (string, string, string, string, error) {
+	// 订单时间
+	orderTime := strkit.SubStr(outTradeNo, 0, 14)
+	if orderTime == "" || len(orderTime) != 14 {
+		return "", "", "", "", logiccode.New(120017, "依据订单号获取日期错误outTradeNo="+outTradeNo)
+	}
+	// 订单年份
+	orderYear := strkit.SubStr(outTradeNo, 0, 4)
+	// 订单月份
+	orderMonth := strkit.SubStr(outTradeNo, 0, 6)
+	// 订单日期
+	orderDay := strkit.SubStr(outTradeNo, 0, 8)
+	// 订单表
+	orderTable := "order_status_" + orderMonth
+
+	return orderTable, orderYear, orderMonth, orderDay, nil
+}
+
+// 根据订单号获取分表信息
+// @outTradeNo 商户订单号
+// 返回值
+// 1:中控订单退款信息存储对应的表名
+// 2:生成订单的年份 YYYY
+// 3:生成订单的月份 YYYYMM
+// 4:生成订单的日期 YYYYMMDD
+func GetRefundTabelInfoWithYearByOutTradeNo(outTradeNo string) (string, string, string, string, error) {
+	// 订单时间
+	orderTime := strkit.SubStr(outTradeNo, 0, 14)
+	if orderTime == "" || len(orderTime) != 14 {
+		return "", "", "", "", logiccode.New(120017, "依据订单号获取日期错误outTradeNo="+outTradeNo)
+	}
+	// 订单年份
+	orderYear := strkit.SubStr(outTradeNo, 0, 4)
+	// 订单月份
+	orderMonth := strkit.SubStr(outTradeNo, 0, 6)
+	// 订单日期
+	orderDay := strkit.SubStr(outTradeNo, 0, 8)
+	// 订单表
+	orderTable := "order_refund_" + orderYear
+
+	return orderTable, orderYear, orderMonth, orderDay, nil
+}
